35daa21caaf86538a8fe3430b0c80d33ced0b983
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 17:32:02 +0200

    changement formulair rapport

--- vues/v_formulaireRapport.php
+++ vues/v_formulaireRapport.php
@@ -8,22 +8,24 @@
             </p>
         </div>
         <div class="py-lg-5 py-3">
-            <form action="index.php?uc=rapport&action=saisit" method="post" class="formulaire-recherche col-12 m-0 d-flex justify-content-between align-content-star p-3">
+            <form action="index.php?uc=rapport&action=saisit" method="post" class="formulaire-recherche col-12 m-0 d-flex justify-content-between align-content-star flex-column p-3">
                 <p class="flex-fill redstar">
                     Champs obligatoires
                 </p>
-                <label class="titre-formulaire" for="listemedoc">Rapport de visite :</label>
-                <select required name="praticien" class="form-select mt-3">
-                    <option value class="text-center">- Choisissez un praticien -</option>
-                    <?php
-                    foreach ($praticiens as $praticien) {
-                    ?>
-                        <option value="" class="form-control"></option>
-                    <?php
-                    }
-                    ?>
-                </select>
-                <input class="btn btn-info text-light valider" type="submit" value="Afficher les informations">
+                <div class="d-flex justify-content-between align-content-star flex-column">
+                    <label class="titre-formulaire" for="listemedoc">Rapport de visite :</label>
+                    <select required name="praticien" class="form-select mt-3">
+                        <option value class="text-center">- Choisissez un praticien -</option>
+                        <?php
+                        foreach ($praticiens as $praticien) {
+                        ?>
+                            <option value="" class="form-control"></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                    <input class="btn btn-info text-light valider" type="submit" value="Afficher les informations">
+                </div>
             </form>
         </div>
     </div>
