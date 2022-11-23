903dd8e50d518343eb4c73b96eb32a19a174ee4b
Mathys FERRE <ferre@infogestion.local>
2022-11-23 17:15:11 +0100

    Patch d'accessibilité de gerer medecin

--- controleur/c_medecins.php
+++ controleur/c_medecins.php
@@ -4,30 +4,26 @@ if (!isset($_REQUEST['action']) || empty($_REQUEST['action'])) {
 } else {
     $action = $_REQUEST['action'];
 }
-if ($_SESSION['habilitation'] == 2) { // Si l'utilisateur possède les droits nécécessaires
-    switch ($action) {
-        case 'formulairemedecin': {
-                if (!isset($_REQUEST['medecin'])) {
-                    $result = getMedecinRegion($_SESSION['region']);
-                    include("vues/v_formulaireMedecin.php");
-                }
-                break;
+switch ($action) {
+    case 'formulairemedecin': {
+            if (!isset($_REQUEST['medecin'])) {
+                $result = getMedecinRegion($_SESSION['region']);
+                include("vues/v_formulaireMedecin.php");
             }
-        case 'gerermedecin': {
-                if (isset($_POST['formulaire_medecin'])) { //Si le délégué visiteur à changé les valeurs d'un médecin
-                    gererUnMedecin($_POST['id_medecin'], $_POST['nom_medecin'], $_POST['prenom_medecin'], $_POST['adresse_medecin'], $_POST['cp_medecin'], $_POST['ville_medecin'], $_POST['coeffNotoriete'], $_POST['type_code'], $_POST['coeffConfiance'], $_POST['formulaire_medecin']);
-                    if (!isset($e)) { // S'il n'y a pas d'erreurs lors de l'éxécution de la fonction gererUnMedecin
-                        echo 'Modifications effectués';
-                    };
-                } else {
-                    if (isset($_REQUEST['medecin'])) {
-                        $medecin = getAllInformationsMedecin($_REQUEST['medecin']);
-                        include("vues/v_gererMedecin.php");
-                        break;
-                    }
+            break;
+        }
+    case 'gerermedecin': {
+            if (isset($_POST['formulaire_medecin'])) { //Si le délégué visiteur à changé les valeurs d'un médecin
+                gererUnMedecin($_POST['id_medecin'], $_POST['nom_medecin'], $_POST['prenom_medecin'], $_POST['adresse_medecin'], $_POST['cp_medecin'], $_POST['ville_medecin'], $_POST['coeffNotoriete'], $_POST['type_code'], $_POST['coeffConfiance'], $_POST['formulaire_medecin']);
+                if (!isset($e)) { // S'il n'y a pas d'erreurs lors de l'éxécution de la fonction gererUnMedecin
+                    echo 'Modifications effectués';
+                };
+            } else {
+                if (isset($_REQUEST['medecin'])) {
+                    $medecin = getAllInformationsMedecin($_REQUEST['medecin']);
+                    include("vues/v_gererMedecin.php");
+                    break;
                 }
             }
-    }
-} else {
-    header('Location: index.php?uc=accueil');
+        }
 }
